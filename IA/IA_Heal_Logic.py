c617f97e8162107b601c8abc7113153cc8451847
Dmorim <92826578+Dmorim@users.noreply.github.com>
2024-12-10 11:12:53 -0300

    Create IA_Heal_Logic.py

---
+++ IA/IA_Heal_Logic.py
@@ -0,0 +1,18 @@
+
+
+class Heal_Logic:
+    def __init__(self, player):
+        self.player = player
+
+
+"""
+Fatores a serem considerados:
+- Diferença entre vida atual e vida máxima
+- Tamanho do exército a ser curado
+- Nível da província atual
+- Combinação dos status de ataque e defesa
+- Província vizinha em batalha
+- Tempo necessário para curar o exército
+- Quantidade de exércitos inimigos nas províncias vizinhas.
+
+"""
